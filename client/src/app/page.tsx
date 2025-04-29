e0a741121e9f7004793e054da6fdecd38f48d8d0
Aman Ramoliya <aman.r@incubyte.co>
2025-04-29 16:17:35 +0530

    ENH: fix client build

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 import { HomePage } from "@/components/HomePage";
 import { Flex } from "@chakra-ui/react";
-import type { AppProps } from "next/app";
 
-export default function Home({ Component, pageProps }: AppProps) {
+export default function Home() {
   return (
     <Flex w={"100%"} direction={"column"} py={4}>
       <HomePage></HomePage>
